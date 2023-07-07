0f8aaf0a8848115861a36bfdff11ae3bb8ffbdb0
Eduardo Cardoso <educardosorv.carmo@gmail.com>
2023-07-07 12:46:37 -0300

    feat: adicionei rotas para listar um ou todos usuarios

--- src/main/java/com/example/todo/controllers/UserController.java
+++ src/main/java/com/example/todo/controllers/UserController.java
@@ -3,25 +3,45 @@ package com.example.todo.controllers;
 import com.example.todo.dtos.UserDto;
 import com.example.todo.services.UserService;
 import com.example.todo.models.UserModel;
+import com.example.todo.services.exceptions.NotFoundException;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping(value = "/users")
 public class UserController {
 
   @Autowired
   private UserService service;
 
-  @PostMapping("/users")
+  @PostMapping
   public ResponseEntity<UserModel> createUser(@RequestBody @Valid UserDto userDto) {
     var user = new UserModel();
     BeanUtils.copyProperties(userDto, user);
     return ResponseEntity.status(HttpStatus.CREATED).body(service.createUser(user));
+  }
+
+  @GetMapping
+  public ResponseEntity<List<UserModel>> getAllUsers() {
+    var users = service.getAllUsers();
+    return ResponseEntity.status(HttpStatus.OK).body(users);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Object> getOneUser(@PathVariable Long id) {
+    try {
+      var user = service.getOneUser(id);
+      return ResponseEntity.ok(user);
+    } catch (NotFoundException e) {
+      var errorMessage = e.getMessage();
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
+    }
   }
 }
--- src/main/java/com/example/todo/services/UserService.java
+++ src/main/java/com/example/todo/services/UserService.java
@@ -2,8 +2,12 @@ package com.example.todo.services;
 
 import com.example.todo.models.UserModel;
 import com.example.todo.repositories.UserRepository;
+import com.example.todo.services.exceptions.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,4 +19,15 @@ public class UserService {
     return repository.save(user);
   }
 
+  public List<UserModel> getAllUsers() {
+    return repository.findAll();
+  }
+
+  public UserModel getOneUser(Long id) {
+    Optional<UserModel> user = repository.findById(id);
+    if (user.isEmpty()) {
+      throw new NotFoundException("User not found");
+    }
+    return user.get();
+  }
 }
